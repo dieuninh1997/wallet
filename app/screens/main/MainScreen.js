96740742ec9a74c81f1c01f187ba9103db3881dd
Thanh Tran <thanh.tran@sotatek.com>
2018-10-09 15:30:59 +0700

    Refactor tabs

--- app/screens/main/MainScreen.js
+++ app/screens/main/MainScreen.js
@@ -16,88 +16,18 @@ const MainScreen = TabNavigator(
   {
     DashboardScreen: {
       screen: props => <DashboardScreen {...props} />,
-      navigationOptions: () => ({
-        tabBarLabel: ({ focused }) => (
-          <Text style={focused ? styles.titleTabFocused : styles.titleTab}>
-            Dashboard
-          </Text>
-        ),
-        tabBarIcon: ({ focused }) => (
-          <View style={styles.tabFocused}>
-            <Image
-              style={styles.iconTab}
-              source={focused ? MangoTabImages.dashboardTabFocused : MangoTabImages.dashboardTab}
-            />
-          </View>
-        ),
-      }),
     },
     RequestScreen: {
       screen: RequestScreen,
-      navigationOptions: () => ({
-        tabBarLabel: ({ focused }) => (
-          <Text style={focused ? styles.titleTabFocused : styles.titleTab}>
-            Request
-          </Text>
-        ),
-        tabBarIcon: ({ focused }) => (
-          <View>
-            <Image
-              style={styles.iconTab}
-              source={focused ? MangoTabImages.requestTabFocused : MangoTabImages.requestTab}
-            />
-          </View>
-
-        ),
-      }),
     },
     SendScreen: {
       screen: SendScreen,
-      navigationOptions: () => ({
-        tabBarLabel: ({ focused }) => (
-          <Text style={focused ? styles.titleTabFocused : styles.titleTab}>
-            Send
-          </Text>
-        ),
-        tabBarIcon: ({ focused }) => (
-          <Image
-            style={styles.iconTab}
-            source={focused ? MangoTabImages.sendTabFocused : MangoTabImages.sendTab}
-          />
-        ),
-      }),
     },
     TransactionScreen: {
       screen: TransactionsScreen,
-      navigationOptions: () => ({
-        tabBarLabel: ({ focused }) => (
-          <Text style={focused ? styles.titleTabFocused : styles.titleTab}>
-            Transactions
-          </Text>
-        ),
-        tabBarIcon: ({ focused }) => (
-          <Image
-            style={styles.iconTab}
-            source={focused ? MangoTabImages.transactionTabFocused : MangoTabImages.transactionTab}
-          />
-        ),
-      }),
     },
   },
   {
-    tabBarOptions: {
-      activeTintColor: '#FFF',
-      inactiveTintColor: '#FFF',
-      showLabel: true,
-      style: {
-        backgroundColor: '#f3f5fa',
-        height: scale(60),
-      },
-      labelStyle: {
-        fontSize: scale(11),
-        paddingBottom: scale(5),
-      },
-    },
     animationEnabled: false,
     swipeEnabled: false,
     initialRouteName: 'DashboardScreen',
@@ -137,24 +67,3 @@ MainScreen.navigationOptions = ({ navigation }) => {
 };
 
 export default MainScreen;
-
-const styles = ScaledSheet.create({
-  viewFocused: {
-    // backgroundColor: 'red',
-    width: '100@s',
-    height: '50@s',
-  },
-  titleTab: {
-    fontSize: '12@s',
-    textAlign: 'center',
-  },
-  titleTabFocused: {
-    fontSize: '12@s',
-    textAlign: 'center',
-    color: '#F5C400',
-  },
-  iconTab: {
-    width: '30@s',
-    height: '30@s',
-  },
-});

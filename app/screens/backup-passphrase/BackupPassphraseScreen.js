095ccb9fce5b32a0255826d9ca49e82c625b0d06
Sotatek-HoaLe <hoa.le@sotatek.com>
2018-10-09 15:11:33 +0700

    style screen

--- app/screens/backup-passphrase/BackupPassphraseScreen.js
+++ app/screens/backup-passphrase/BackupPassphraseScreen.js
@@ -11,12 +11,21 @@ import QRCode from 'react-native-qrcode-svg';
 import I18n from '../../i18n/i18n';
 import ScaledSheet from '../../libs/reactSizeMatter/ScaledSheet';
 import MangoGradientButton from '../common/MangoGradientButton';
-import { CommonColors } from '../../utils/CommonStyles';
+import MangoBackButton from '../common/MangoBackButton';
+import { CommonStyles, CommonColors, CommonSize } from '../../utils/CommonStyles';
 import { scale } from '../../libs/reactSizeMatter/scalingUtils';
 import AppPreferences from '../../utils/AppPreferences';
 import AppConfig from '../../utils/AppConfig';
 
 class BackupPassphraseScreen extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerLeft: navigation.getParam('isFromSettingScreen')? <MangoBackButton navigation={navigation} /> : <View />,
+    title: I18n.t('backupPassphrase.title'),
+    headerTitleStyle: CommonStyles.headerTitle,
+    headerStyle: CommonStyles.header,
+    headerRight: <View />,
+  })
+
   constructor(props) {
     super(props);
     this.state = {
@@ -31,45 +40,66 @@ class BackupPassphraseScreen extends Component {
     AppPreferences.showToastMessage(I18n.t('request.copied'));
   }
 
-  _navigateScreen = () => {
+  _onBtnNext = () => {
     const { navigation } = this.props;
     navigation.navigate('AddPinScreen');
   }
 
   _renderQrCodeSection = () => {
     const { mnemoric } = this.state;
+    console.log('Mnemoric: ', mnemoric);
 
     return (
       <View style={styles.qrCodeSectionContainer}>
+        <View style = {styles.viewMnemoric}>
+          <Text style = {[styles.txtNote, {color: '#000000'}]}>
+              {I18n.t("backupPassphrase.note")}
+          </Text>
+        </View>
+        
         <View style={styles.qrCodeContainer}>
           {!mnemoric ? null : (
             <QRCode
               value={mnemoric}
-              size={scale(260)}
+              size={scale(200)}
             />
           )}
         </View>
+
         <View style={styles.addressContainer}>
-          <Text>{mnemoric}</Text>
+          <Text style = {styles.txtMnemoric}>{mnemoric}</Text>
         </View>
+
+        <View style = {styles.viewMnemoric}>
+          <Text style = {styles.txtNote}>
+              {I18n.t("backupPassphrase.important")}
+          </Text>
+        </View>
+        
       </View>
     );
   }
 
   _renderBtnSection = () => (
-    <View style={styles.groupBtnContainer}>
-      <TouchableOpacity
-        activeOpacity={0.5}
-        style={styles.btnUpContainer}
-        onPress={() => this._navigateScreen()}
-      >
-        <Image
-          source={require('../../../assets/go-right/go-right.png')}
-          style={styles.btnUpImage}
-        />
-      </TouchableOpacity>
+    <View style={[styles.groupBtnContainer, {justifyContent: 'space-between'}]}>
       <MangoGradientButton
-        btnText={I18n.t('request.copyAddress')}
+        btnText={I18n.t('backupPassphrase.btnCopy')}
+        btnStyle={styles.btnCopyAddress}
+        onPress={() => this._handleCopyAddress()}
+      />
+
+      <MangoGradientButton
+        btnText={I18n.t('backupPassphrase.btnNext')}
+        btnStyle={styles.btnCopyAddress}
+        onPress={() => this._onBtnNext()}
+      />
+    </View>
+  )
+
+  _renderBtnSectionSetting = () => (
+    <View style={[styles.groupBtnContainer, {justifyContent: 'center'}]}>
+      <MangoGradientButton
+        btnText={I18n.t('backupPassphrase.btnCopy')}
         btnStyle={styles.btnCopyAddress}
         onPress={() => this._handleCopyAddress()}
       />
@@ -77,6 +107,8 @@ class BackupPassphraseScreen extends Component {
   )
 
   render() {
+    const {isFromSettingScreen} = this.props.navigation.getParam('isFromSettingScreen', false);
+
     return (
       <View style={styles.container}>
         <ScrollView
@@ -84,7 +116,7 @@ class BackupPassphraseScreen extends Component {
         >
           <View style={styles.contentContainer}>
             {this._renderQrCodeSection()}
-            {this._renderBtnSection()}
+            {isFromSettingScreen ? this._renderBtnSectionSetting():this._renderBtnSection()}
           </View>
         </ScrollView>
       </View>
@@ -97,7 +129,7 @@ const styles = ScaledSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: '#F5F7FA',
+    backgroundColor: CommonColors.screenBgColor,
   },
 
   contentContainer: {
@@ -111,8 +143,8 @@ const styles = ScaledSheet.create({
   },
 
   qrCodeContainer: {
-    width: '320@s',
-    height: '320@s',
+    width: '260@s',
+    height: '260@s',
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: '8@s',
@@ -125,21 +157,28 @@ const styles = ScaledSheet.create({
     height: '260@s',
   },
 
+  viewMnemoric: {
+    marginLeft: '20@s',
+    marginRight: '20@s',
+    marginBottom: '20@s',
+    //height: '300@s',
+  },
+
   addressContainer: {
-    width: '280@s',
-    borderBottomLeftRadius: '8@s',
-    borderBottomRightRadius: '8@s',
-    paddingHorizontal: '16@s',
-    paddingVertical: '14@s',
+    width: '260@s',
+    borderRadius: '8@s',
     backgroundColor: '#E4E9F1',
     marginBottom: '24@s',
+    marginTop: '10@s',
+    marginRight: '20@s',
+    marginLeft: '20@s',
   },
 
   // Section button
   groupBtnContainer: {
     flexDirection: 'row',
-    width: '320@s',
-    justifyContent: 'space-between',
+    marginLeft: '20@s',
+    marginRight: '20@s',
   },
 
   btnUpContainer: {
@@ -158,8 +197,20 @@ const styles = ScaledSheet.create({
   },
 
   btnCopyAddress: {
-    width: '220@s',
+    width: '140@s',
     marginBottom: '10@s',
-    marginRight: '4@s',
   },
+
+  txtNote: {
+    fontSize: CommonSize.inputFontSize,
+    color: CommonColors.highlightRed,
+  },
+
+  txtMnemoric: {
+    fontSize: '16@s',
+    color: CommonColors.highlightBlue,
+    fontWeight: 'bold',
+    paddingHorizontal: '10@s',
+    paddingVertical: '10@s',
+  }
 });
